69c9ac4d4eb6e720197f5d50b2bb04f0b2b64722
Rohit Rajsurya Prasad <rohit.rajsurya10@gmail.com>
2024-01-30 23:41:08 +0530

    custom token contract

---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import * as dotenv from "dotenv";
+// import "@nomicfoundation/hardhat-toolbox";
+// import "@nomicfoundation/hardhat-foundry";
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
+
+dotenv.config();
+
+
+module.exports ={
+    defaultNetwork: "zksync",
+    networks: {
+        hardhat: {
+        },
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/wJ4MOyRL_MQ9a3jJiFVpBGrfZf9jmVKL`,
+            accounts: [`${process.env.PRIVATE_KEY_DEPLOY}`]
+        },
+        zksync: {
+            url: `https://sepolia.era.zksync.dev`,
+            ethNetwork: "sepolia", 
+            zksync: true,
+            accounts: [`${process.env.PRIVATE_KEY_DEPLOY}`],
+            verifyURL: 'https://explorer.sepolia.era.zksync.dev/contract_verification'
+
+        }
+    },
+    solidity: {
+        compilers: [
+            {
+              version: "0.8.19",
+            }
+          ],
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
+    zksolc: { // need to reference zksolc compiler
+        compilerSource: 'binary',
+        version: "1.3.22",
+        settings: {
+           
+      },
+    }
+}
---
+++ src/ManualToken.sol
@@ -0,0 +1,28 @@
+//SPDX 
+pragma solidity ^0.8.19;
+
+contract ManualToken{
+
+    mapping(address=>uint256) private s_balances;
+
+    function name() public pure returns (string memory){
+        return "Manual Token";
+    }
+
+    function totalSupply() public pure returns (uint256){
+        return 100 ether;
+    }
+    function decimals() public pure returns (uint8){
+        return 18;
+    }
+    function balanceOf(address _owner) public view returns (uint256){
+        return s_balances[_owner];
+    }
+    function transfer(address _to, uint256 _value) public returns (bool){
+        require(s_balances[msg.sender] >= _value);
+        s_balances[msg.sender] -= _value;
+        s_balances[_to] += _value;
+        return true;
+    }
+
+}
